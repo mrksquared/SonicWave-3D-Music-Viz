38c24d33ba663aa008c403bff9bc8db59cb58670
mrksquared <key@gatech.edu>
2020-02-12 18:30:56 -0500

    start on c#

---
+++ spotify_get_data.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+  public Text song_name;
+  public Text artist_name;
+
+  
+}
