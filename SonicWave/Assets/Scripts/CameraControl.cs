9213b8a88168db0b66dd6d99485bea432e972984
mrksquared <key@gatech.edu>
2020-04-13 14:48:29 -0400

    working on interaction & camera control

---
+++ SonicWave/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    private float moveSpeed = 0.5f;
+    private float scrollSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0) {
+              transform.position += moveSpeed * new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+          }
+
+          if (Input.GetAxis("Mouse ScrollWheel") != 0) {
+              transform.position += scrollSpeed * new Vector3(0, -Input.GetAxis("Mouse ScrollWheel"), 0);
+          }
+    }
+}
